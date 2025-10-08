a8ac72a3bbe5425a264a3a2f50d595dc6665da43
Sean Meek <smeek123@users.noreply.github.com>
2025-10-08 18:19:54 -0400

    Initial Commit

---
+++ vacaition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vacaition
+//
+//  Created by Sean Meek on 10/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vacaition/vacaitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  vacaitionApp.swift
+//  vacaition
+//
+//  Created by Sean Meek on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct vacaitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
